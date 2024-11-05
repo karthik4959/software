d1bd363d9da70dfd719f149a5bb331c2f9f0610c
Karthik Reddy <svb491@gmail.com>
2024-11-05 02:38:37 +0100

    test_main

--- test_word.py
+++ test_word.py
@@ -1,10 +1,12 @@
 from word import WordCounter
 
-wordcounter = WordCounter
+wordcounter = WordCounter()
 
 # Test case for the uppercase count function
+
 def test_count_uppercase():
-    text = "Hello, World" # Input for the test
-    expected_result = 3 # Expected result
-    result = wordcounter.count_uppercase(text)
+    text = "HEllo, World" # In put for the test
+    expected_result = 3 # Expected out put
+    result = wordcounter.count_uppercase(text) # Result for the test
     assert result == expected_result, f"Expected: {expected_result}, but got: {result}"
+
